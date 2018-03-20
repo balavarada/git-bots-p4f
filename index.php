f18de92d62b344ddae8add9dbad71179a40ace9b
Varadarajan <Balakrishnan.Varadarajan@ad.agi>
2018-03-20 18:27:16 +0530

    Initial commit 1 .

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
 
-echo "Hello World - Hello bots";
+//echo "Hello World - Hello bots";
 
-echo "<pre>";
-print_r($_POST); //getCategory
+//echo "<pre>";
+//print_r($_POST); //getCategory
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+
+print_r($_POST);
+$action = $_POST['result']['action'];
+
+if(action = "getCategory") {
+
+
+}
 
 
 ?>
