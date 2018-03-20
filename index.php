8ba5e4cb2e500db0fdf0d3873e52a84b71c84cf1
Varadarajan <Balakrishnan.Varadarajan@ad.agi>
2018-03-20 18:41:46 +0530

    Initial commit 1 .

--- index.php
+++ index.php
@@ -9,14 +9,25 @@ $_POST = json_decode(file_get_contents('php://input'), true);
 
 
 print_r($_POST);
-$action = $_POST['result']['action'];
+$action     = $_POST['result']['action'];
+$parameters = $_POST['result']['parameters'];
+$return     = "Default Response from Bot-Web-Hook";
 
-if($action == "getCategory") {
-
-
+if ($action == "getCategory") {
+    
+    if ($parameters['facts-category'] == 'Account') {
+        $return = "Link your core system account from here";
+        
+    } else if ($parameters['facts-category'] == 'Casino') {
+        
+        $return = "Get the updates from Hotel, Clubs, Bar, restaurents from here";
+    } else if ($parameters['facts-category'] == 'Play') {
+        $return = "Play your Slots and Table games from here";
+    }
 }
+
+echo $return;
+die;
 
 
 ?>
-
-
