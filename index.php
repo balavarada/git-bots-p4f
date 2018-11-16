772b11a843c558fbc6b7a55257f2e96bc21bc10d
Varadarajan <Balakrishnan.Varadarajan@ad.agi>
2018-11-16 15:39:50 +0530

    Updates on action navigatetogame.

--- index.php
+++ index.php
@@ -3,7 +3,40 @@ $_POST = json_decode(file_get_contents('php://input'), true);
 
 $action     = $_POST['result']['action'];
 $parameters = $_POST['result']['parameters'];
-
+$games = array(
+    'junglewild',
+    'bierhaus200',
+    'rocketreturns',
+    'spiritguidepanda',
+    'emeraldfalls',
+    'tropicalfish',
+    'monopolypartytrain-desktop',
+    'quickhitbgfgf',
+    'dragonsfire',
+    'wickedbeauty',
+    'kisshydra',
+    'cashwizard',
+    'epicmonopoly',
+    'empire',
+    'margaritaville',
+    'bigredlantern',
+    'ladyrobinhood',
+    'giantsgold',
+    'romeandegypt',
+    'superjackpotparty',
+    'junglewild2',
+    'wildshootout',
+    'superredphoenix',
+    'rainbowriches',
+    'zeus',
+    'mysticalfortunes',
+    'glitz',
+    'lancelot',
+    'pridenprey',
+    'greatwall2',
+    'romanchariots',
+    'lockitlinkdiamonds'
+);
 
 if ($action == "getCategory") {
     switch ($parameters['facts-category']) {
@@ -18,9 +51,15 @@ if ($action == "getCategory") {
             $_return = "Default Response from Bot-Web-Hook";
             break;
     }
+    if ($parameters['game-name']) {
+        foreach ($games as $parameters['game-name']) {
+            if (strpos($value, 'green') !== false) { $results[] = $value; }          
+        }
+    }
+    if( empty($results) ) { $_return = 'No matches found.'; }
+    else { $_return = "'green' was found in: "; }
 
-
-    print_r(
+   // print_r(
 
 '{
 	"speech": "' . $_return . '",
